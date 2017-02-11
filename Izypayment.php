115fe2cb4ffd1e5fdc6bf252504dd1c58ac9bf48
eddyTheDove <EddyTheDove@users.noreply.github.com>
2017-02-12 06:38:05 +1100

    Update Izypayment.php

--- Izypayment.php
+++ Izypayment.php
@@ -15,20 +15,9 @@ class Izypayment {
     *
     */
     public function pay($params = []) {
-        $paymentUrl = self::END_POINT . '/pay';
-        $data = [];
-        $data = $this->apiCurl($url, $params);
-        
+        $data = $this->apiCurl('/pay', $params);
         $response_json = (array)json_decode($data['response'], true);
-        $response = $response_json['content'];
-        $statusCode = $response_json['statusCode'];
-
-        if ($statusCode != 1) {
-            $error = $response_json['content']
-            /**
-             * TODO when error in the transaction
-             */
-        }
+        return $response_json;
     }
 
 
@@ -41,6 +30,7 @@ class Izypayment {
      */
     private function apiCurl($url, $params)
     {
+        $url = self::END_POINT . $url;
         $curl = curl_init();
 
         $curl_params = array(
